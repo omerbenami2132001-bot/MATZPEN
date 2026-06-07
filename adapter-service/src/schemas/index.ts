ef113acadfb256acf85c03555c85092f8c7de08d
omer ben ami <omerbenami2132001@gmail.com>
2026-06-07 14:54:32 +0300

    after daniel cr

--- adapter-service/src/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./api.schemas";
-export * from "./adapter.schemas";
-export * from "./kafka.schemas";
-export * from "./s3.schemas";
-export * from "./metadata.schemas";
--- adapter-service/src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const SOURCE_NAME = "adapter-service";
-export const S3_BUCKET = process.env.S3_BUCKET || "raw-data";
-export const AWS_REGION = process.env.AWS_REGION || "us-east-1";
-export const KAFKA_TOPIC = "adapter.file.downloads";
-
-export const METADATA_API_1_PREFIX = "ex";
-
-export const METADATA_API_2_PREFIX = "ab";
-export const METADATA_API_2_FIELDS = ["*"];
